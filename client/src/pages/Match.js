2cd2ab6739ce25502c1f1c8e55b01a4a86e878c3
Cordell <cordellbonnieux@gmail.com>
2022-10-29 22:04:11 -0700

    Added canvas.

--- client/src/pages/Match.js
+++ client/src/pages/Match.js
@@ -1,3 +1,40 @@
+import { useRef, useEffect, useState } from 'react'
 export default function Match(props) {
-    return <>match</>
+    const [ canvasSize, setCanvasSize ] = useState({
+        x: document.documentElement.clientWidth,
+        y: document.documentElement.clientHeight
+    })
+    const canvasRef = useRef(null)
+
+    function move(unit, xchange, ychange) {
+
+    }
+
+    useEffect(() => {
+        setCanvasSize({
+            x: document.documentElement.clientWidth,
+            y: document.documentElement.clientHeight
+        })
+        const canvas = canvasRef.current
+        const context = canvas.getContext('2d')
+        let frameCount = 0
+        let animationFrameId
+
+        const render = () => {
+            frameCount++
+            // draw stuff
+            animationFrameId = window.requestAnimationFrame(render)
+        }
+        render()
+
+        return () => {
+            window.cancelAnimationFrame(animationFrameId)
+        }
+    }, [])
+
+    return (
+        <main>
+            <canvas ref={canvasRef} height={canvasSize.y} width={canvasSize.x} />
+        </main>
+    )
 }
