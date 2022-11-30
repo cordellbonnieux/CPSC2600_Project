af1c11b4fd74034cd9677ff0af96b9bcbd68a7be
Cordell <cordellbonnieux@gmail.com>
2022-11-30 12:25:28 -0800

    Added visual que to unit cards for units awaiting orders

--- client/src/components/UnitsOverlayUnit.js
+++ client/src/components/UnitsOverlayUnit.js
@@ -4,8 +4,8 @@ export default function UnitsOverlayUnit(props) {
         <ul onClick={() => setSelectionFromUI(index)} className={selected ? 'selected' : ''}>
             <li>unit #{index}</li>
             <li>HP: {data.hp}</li>
-            <li>Attack: {data.attacked ? 'complete' : 'awaiting orders'}</li>
-            <li>Move: {data.moved ? 'complete' : 'awaiting orders'}</li>
+            <li className={data.attacked ? '' : 'waiting'}>Attack: {data.attacked ? 'complete' : 'awaiting orders'}</li>
+            <li className={data.moved ? '' : 'waiting'}>Move: {data.moved ? 'complete' : 'awaiting orders'}</li>
         </ul>
     )
 }
