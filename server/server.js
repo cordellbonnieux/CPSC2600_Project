fcad4393e03374f67ad4e9dec0f5c6a29b84bd7d
Cordell <cordellbonnieux@gmail.com>
2022-12-01 12:29:55 -0800

    added cors origin to server in server.js

--- server/server.js
+++ server/server.js
@@ -4,7 +4,9 @@ const express = require('express')
 const app = express()
 
 const cors = require('cors')
-app.use(cors())
+app.use(cors({
+  origin: ['https://cordellbonnieux.github.io/CPSC2600_Project/']
+}))
 app.use(express.json())
 
 // routes
